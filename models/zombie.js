8446785823ebd081d9719d165254dfd5d4fd636a
Jared Salazar Martinez <33723377+TateTipycsnow@users.noreply.github.com>
2020-03-11 19:25:39 -0600

    La wea que no sirve

--- models/zombie.js
+++ models/zombie.js
@@ -4,16 +4,16 @@ var modelSchema = mongoose.Schema({
     Name: {
         type: String,
         required: [true, "El nombre es obligatorio"],
-        minlength: [6, "El nombre es muy corto"],
+        minlength: [5, "El nombre es muy corto"],
         maxlength: [20, "El nombre es muy largo"]
     },
     Mail: {
         type: String,
         required: [true, "El e-mail es obligatorio"]
-    }, 
+    },
     Type: {
         type: String,
-        enum: ["Alumno","Maestro"]
+        enum: ["Alumno", "Maestro"]
     }
 });
 
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,32 @@ let router = express.Router();
 let Zombie = require('../models/zombie');
 let Cerebro = require('../models/cerebro');
 
+router.post('/zombies/new', async function(req, res) {
+    var data = req.body;
+
+    var nuevoZombie = new Zombie({
+        Name: data.name,
+        Mail: data.email,
+        Type: data.type
+    });
+
+    nuevoZombie.save(function(error) {
+        let Campos = ["Name", "Mail", "Type"];
+
+        if (error) {
+            for (var i = 0; i < Campos.length; i++) {
+                var Campo = Campos[i];
+                if (error.errors[Campo]) {
+                    mensajeResultante = error.errors[Campo].message;
+                    res.status(500).json({ Alerta: "alert alert-danger", Mensaje: mensajeResultante, Hide: '' });
+                }
+            }
+        } else {
+            res.status(200).json({ Alerta: "alert alert-success", Mensaje: "Zombie agregado", Hide: '' });
+        }
+    });
+});
+
 router.get('/zombies', async(req, res) => {
     Zombie.find().exec((error, zombies) => {
         if (!error) {
